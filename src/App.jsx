65ed7aca98ab55c6b3d2ec34b9dd4454d655a372
Salauddin Ahmed <110757660+Salauddin2k3@users.noreply.github.com>
2024-07-01 00:24:59 +0600

    start portfolio building

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <h2 className="text-red-500">Test</h2>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/Route/Route.jsx
@@ -0,0 +1,26 @@
+
+import {
+    createBrowserRouter,
+    RouterProvider,
+} from "react-router-dom";
+import Root from "../Components/Root/Root";
+import Home from "../Components/Home/Home";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Root></Root>,
+        children: [
+            {
+                path: "/",
+                element: <Home></Home>
+            },
+        ]
+    },
+]);
+
+const Route = () => {
+    return <RouterProvider router={router} />;
+};
+
+export default Route;
