efcb077a682c6c16ba04ff4ee5d6a5b27f659aa4
Salauddin Ahmed <110757660+Salauddin2k3@users.noreply.github.com>
2024-07-04 11:43:53 +0600

    Add: Social Links

--- src/Components/Home/Banner/Banner.jsx
+++ src/Components/Home/Banner/Banner.jsx
@@ -4,6 +4,8 @@ import { FaFacebook, FaGithub, FaGoogle, FaLinkedinIn } from "react-icons/fa";
 import { FaSquareXTwitter } from "react-icons/fa6";
 import Lottie from "lottie-react";
 import LottieGif from "../../../../public/typing-lottie.json"
+import { Link } from "react-router-dom";
+import myResume from "../../../../public/resume.pdf"
 
 
 const Banner = () => {
@@ -24,15 +26,27 @@ const Banner = () => {
                 </div>
                 <h2 className="text-[#8d8d8d] mt-5 text-center lg:text-left lg:text-2xl">My goal is to excel in web development through innovative <br /> programming and  delivering top-notch solutions.</h2>
                 <div className="flex justify-center lg:justify-start gap-3 mt-8">
-                    <div className=" w-fit p-2 rounded-full bg-[#0e76a8]"><FaFacebook className="text-2xl lg:text-3xl text-white" /></div>
-                    <div className=" w-fit p-2 rounded-full bg-[#333333]"><FaGithub className="text-2xl lg:text-3xl text-white" /></div>
-                    <div className=" w-fit p-2 rounded-full bg-[#0e76a8]"><FaLinkedinIn className="text-2xl lg:text-3xl text-white" /></div>
-                    <div className=" w-fit p-2 rounded-full bg-[#333333]"><FaSquareXTwitter className="text-2xl lg:text-3xl text-white" /></div>
-                    <div className=" w-fit p-2 rounded-full bg-[#e94435]"><FaGoogle className="text-2xl lg:text-3xl text-white" /></div>
+                    <Link to="https://www.facebook.com/salauddin2k3" target="__blank">
+                        <div className=" w-fit p-2 rounded-full bg-[#0e76a8]"><FaFacebook className="text-2xl lg:text-3xl text-white" /></div>
+                    </Link>
+                    <Link to="https://github.com/salauddin2k3" target="__blank">
+                        <div className=" w-fit p-2 rounded-full bg-[#333333]"><FaGithub className="text-2xl lg:text-3xl text-white" /></div>
+                    </Link>
+                    <Link to="https://www.linkedin.com/in/salauddin2k3/" target="__blank">
+                        <div className=" w-fit p-2 rounded-full bg-[#0e76a8]"><FaLinkedinIn className="text-2xl lg:text-3xl text-white" /></div>
+                    </Link>
+                    <Link to="https://x.com/salauddin2k3" target="__blank">
+                        <div className=" w-fit p-2 rounded-full bg-[#333333]"><FaSquareXTwitter className="text-2xl lg:text-3xl text-white" /></div>
+                    </Link>
+                    <Link to="mailto:salauddin2k3@gmail.com" target="__blank">
+                        <div className=" w-fit p-2 rounded-full bg-[#e94435]"><FaGoogle className="text-2xl lg:text-3xl text-white" /></div>
+                    </Link>
                 </div>
-                <div className="flex items-center justify-center lg:justify-start">
-                    <button className="btn bg-[#f73378] border-none text-white lg:text-lg hover:bg-[#f73378] hover:shadow-lg hover:shadow-[#f73378] mt-8">Download My Resume</button>
-                </div>
+                <a href={myResume} download="Ahmed_Resume.pdf">
+                    <div className="flex items-center justify-center lg:justify-start">
+                        <button className="btn bg-[#f73378] border-none text-white lg:text-lg hover:bg-[#f73378] hover:shadow-lg hover:shadow-[#f73378] mt-8">Download My Resume</button>
+                    </div>
+                </a>
             </div>
             <div className="lg:w-1/2 px-2 flex justify-end bg-[#1d2026]">
                 <div className="">
