f1853022f2280960b1ee516066ce872a17c60670
Salauddin Ahmed <110757660+Salauddin2k3@users.noreply.github.com>
2024-07-04 14:28:37 +0600

    Add: Skills- MERN Stack Web Development

--- src/Components/Skills/Skills.jsx
+++ src/Components/Skills/Skills.jsx
@@ -1,16 +1,22 @@
 import Lottie from "lottie-react";
-import skillLottie from "../../../public/skills-lottie.json"
+import skillLottie from "../../../public/skills-lottie.json";
+import teaCodeLottie from "../../../public/tea-code-lottie.json";
 import { MdOutlineMonitor } from "react-icons/md";
-import { FaHtml5, FaReact } from "react-icons/fa";
+import { FaHtml5, FaNodeJs, FaReact } from "react-icons/fa";
 import { IoLogoCss3 } from "react-icons/io";
 import { RiTailwindCssFill } from "react-icons/ri";
 import { HiLightningBolt } from "react-icons/hi";
+import { SiExpress } from "react-icons/si";
+import { BiLogoMongodb } from "react-icons/bi";
 
 
 const Skills = () => {
     return (
         <div className="text-white">
             <h2 className="text-3xl lg:text-5xl text-center mt-16 lg:mt-28 font-bold">Technologies that I know</h2>
+
+            {/* ----------------First--------------------- */}
+
             <div className="mt-4 lg:mt-20 flex flex-col lg:flex-row gap-10 lg:gap-20">
                 <div className="lg:w-1/2 flex items-center justify-center">
                     <Lottie
@@ -47,6 +53,46 @@ const Skills = () => {
                     </div>
                 </div>
             </div>
+
+            {/* ----------Second------------- */}
+
+            <div className="mt-14 lg:mt-36 flex flex-col lg:flex-row gap-10 lg:gap-20">
+                <div className="lg:w-1/2">
+                    <div className="flex items-center justify-center gap-3">
+                        <MdOutlineMonitor className="text-xl lg:text-4xl text-[#f73378]" />
+                        <h2 className="text-base lg:text-4xl font-bold">MERN Stack Web Development</h2>
+                    </div>
+                    {/* Logos */}
+                    <div className="mt-8 flex justify-center gap-6">
+                        <div className="tooltip" data-tip="React Js"><FaReact className="text-4xl lg:text-6xl text-[#6cd6f3]" /></div>
+                        <div className="tooltip" data-tip="Node Js"><FaNodeJs className="text-4xl lg:text-6xl text-[#59a149]" /></div>
+                        <div className="tooltip" data-tip="Express Js"><SiExpress className="text-4xl lg:text-6xl text-[#f6e01f]" /></div>
+                        <div className="tooltip" data-tip="Mongodb"><BiLogoMongodb className="text-4xl lg:text-6xl text-[#439744]" /></div>
+                    </div>
+                    <div>
+                        <div className="flex  gap-2 ml-6 mt-6">
+                            <div><HiLightningBolt className="text-lg lg:text-xl text-[#f73378]" /></div>
+                            <h2 className="text-[#8d8d8d] lg:text-2xl">As a MERN stack developer, I specialize in building web applications using MongoDB, Express.js, React.js, and Node.js.</h2>
+                        </div>
+                        <div className="flex  gap-2 ml-6 mt-6">
+                            <div><HiLightningBolt className="text-lg lg:text-xl text-[#f73378]" /></div>
+                            <h2 className="text-[#8d8d8d] lg:text-2xl">Using Node.js and Express.js, I implement server-side logic and middleware to handle requests and responses efficiently.</h2>
+                        </div>
+                        <div className="flex  gap-2 ml-6 mt-6">
+                            <div><HiLightningBolt className="text-lg lg:text-xl text-[#f73378]" /></div>
+                            <h2 className="text-[#8d8d8d] lg:text-2xl">With a passion for continuous learning, I actively explore new tools and frameworks to enhance my MERN stack development skills.</h2>
+                        </div>
+                    </div>
+                </div>
+                <div className="lg:w-1/2 flex items-center justify-center">
+                    <Lottie
+                        animationData={teaCodeLottie}
+                        loop={true}
+                        className="w-56 lg:w-96 bg-gray-200 rounded-md"
+                    ></Lottie>
+                </div>
+            </div>
+
         </div>
     );
 };
