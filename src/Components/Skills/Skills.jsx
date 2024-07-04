ef80608c50a1b54a0be6c935a8b31241b9d5e99f
Salauddin Ahmed <110757660+Salauddin2k3@users.noreply.github.com>
2024-07-04 14:54:24 +0600

    Add: Skills-Another Technologies & Tools

--- src/Components/Skills/Skills.jsx
+++ src/Components/Skills/Skills.jsx
@@ -1,13 +1,15 @@
 import Lottie from "lottie-react";
 import skillLottie from "../../../public/skills-lottie.json";
 import teaCodeLottie from "../../../public/tea-code-lottie.json";
+import toolLottie from "../../../public/web-tool-lottie.json";
 import { MdOutlineMonitor } from "react-icons/md";
-import { FaHtml5, FaNodeJs, FaReact } from "react-icons/fa";
+import { FaFigma, FaGithub, FaHtml5, FaNodeJs, FaReact } from "react-icons/fa";
 import { IoLogoCss3 } from "react-icons/io";
 import { RiTailwindCssFill } from "react-icons/ri";
 import { HiLightningBolt } from "react-icons/hi";
-import { SiExpress } from "react-icons/si";
+import { SiExpress, SiNpm } from "react-icons/si";
 import { BiLogoMongodb } from "react-icons/bi";
+import { IoLogoFirebase } from "react-icons/io5";
 
 
 const Skills = () => {
@@ -56,7 +58,7 @@ const Skills = () => {
 
             {/* ----------Second------------- */}
 
-            <div className="mt-14 lg:mt-36 flex flex-col lg:flex-row gap-10 lg:gap-20">
+            <div className="mt-14 lg:mt-36 flex flex-col-reverse lg:flex-row gap-10 lg:gap-20">
                 <div className="lg:w-1/2">
                     <div className="flex items-center justify-center gap-3">
                         <MdOutlineMonitor className="text-xl lg:text-4xl text-[#f73378]" />
@@ -90,6 +92,45 @@ const Skills = () => {
                         loop={true}
                         className="w-56 lg:w-96 bg-gray-200 rounded-md"
                     ></Lottie>
+                </div>
+            </div>
+
+            {/* ----------------Third--------------------- */}
+
+            <div className="mt-14 lg:mt-36 flex flex-col lg:flex-row gap-10 lg:gap-20">
+                <div className="lg:w-1/2 flex items-center justify-center">
+                    <Lottie
+                        animationData={toolLottie}
+                        loop={true}
+                        className="w-56 lg:w-96"
+                    ></Lottie>
+                </div>
+                <div className="lg:w-1/2">
+                    <div className="flex items-center justify-center gap-3">
+                        <MdOutlineMonitor className="text-xl lg:text-4xl text-[#f73378]" />
+                        <h2 className="text-base lg:text-4xl font-bold">Another Technologies & Tools</h2>
+                    </div>
+                    {/* Logos */}
+                    <div className="mt-8 flex justify-center gap-6">
+                        <div className="tooltip" data-tip="Github"><FaGithub className="text-4xl lg:text-6xl text-white"/></div>
+                        <div className="tooltip" data-tip="Firebase"><IoLogoFirebase className="text-4xl lg:text-6xl text-[#ee7e0d]"/></div>
+                        <div className="tooltip" data-tip="NPM"><SiNpm className="text-4xl lg:text-6xl text-[#dd2c35]"/>                        </div>
+                        <div className="tooltip" data-tip="Figma"><FaFigma className="text-4xl lg:text-6xl text-[#1472b7]"/></div>
+                    </div>
+                    <div>
+                        <div className="flex lg:items-center gap-2 ml-6 mt-6">
+                            <div><HiLightningBolt className="text-lg lg:text-xl text-[#f73378]" /></div>
+                            <h2 className="text-[#8d8d8d] lg:text-2xl">Building responsive web front-end using React.</h2>
+                        </div>
+                        <div className="flex lg:items-center gap-2 ml-6 mt-6">
+                            <div><HiLightningBolt className="text-lg lg:text-xl text-[#f73378]" /></div>
+                            <h2 className="text-[#8d8d8d] lg:text-2xl">Focus on creating dynamic and interactive user interfaces.</h2>
+                        </div>
+                        <div className="flex lg:items-center gap-2 ml-6 mt-6">
+                            <div><HiLightningBolt className="text-lg lg:text-xl text-[#f73378]" /></div>
+                            <h2 className="text-[#8d8d8d] lg:text-2xl">Using Tailwind, I build sleek web interfaces quickly.</h2>
+                        </div>
+                    </div>
                 </div>
             </div>
 
